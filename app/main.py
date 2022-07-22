6a35a97e98a01fb23038e2c2a65640c4cfa353cc
bayram <test@test.org>
2022-07-22 14:19:08 +0300

    feat(add): New main.py route /about'

--- app/main.py
+++ app/main.py
@@ -4,4 +4,9 @@ app = Flask(__name__)
  
 @app.route("/")
 def home_view():
-        return "<h1>Hello world</h1>"
+    return "<h1>Hello world</h1>"
+
+
+@app.route("/about")
+def about():
+    return "<h2>About page</h2>"
